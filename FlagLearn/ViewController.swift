abe0135536213b053ebc14991c028b96d12db44c
Benjamin Kent <benjaminfranklinkent@frontier.com>
2021-03-30 18:29:11 -0700

    Initial Commit

---
+++ FlagLearn/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FlagLearn
+//
+//  Created by benjamin kent on 3/30/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
